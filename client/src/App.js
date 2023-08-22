96c90e83f760879535c844a48f1a2da3e39586d0
{alexius <{alexiussavage0@gmail.com}>
2023-08-22 10:00:29 -0700

    adding apolo server and installing some packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Cart from './pages/Cart1'
-import Homepage from './pages/Homepage1';
-import Payment from './pages/Payment1';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Cart from "./pages/Cart1";
+import Homepage from "./pages/Homepage1";
+import Payment from "./pages/Payment1";
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 // If you want to make another page, make sure its capitalized
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-        <Route path ="/" element={<Homepage />} />
-        <Route path ="/cart" element={<Cart />} />
-        <Route path ="/Payment" element={<Payment />} />
-        </Routes>
-      </Router>
-  
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/Payment" element={<Payment />} />
+          </Routes>
+        </Router>
+      </div>
+    </ApolloProvider>
   );
 }
 
