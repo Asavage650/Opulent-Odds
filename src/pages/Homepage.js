6ede19a4b1eb650bf453b0a323f20370acde42cb
Cecilj24 <clangba@yahoo.com>
2023-08-21 20:44:52 -0700

    added the routes to app.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,2 +1,9 @@
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>Homepage</div>
+  )
+}
+
+export default Homepage
