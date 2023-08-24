6669f0c3f5d217ecd79df39715f139669aa11270
Whynotwynaut <125845043+EHernandez1213@users.noreply.github.com>
2023-08-23 20:46:26 -0700

    added typedefs removed node modules ;

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,37 @@
+const typeDefs = `
+type User{
+    _id: ID!
+    username: String!
+    email: String!
+    postedItems: [Item]
+    cart: [Item]
+}
+type Item{
+    itemId: String!
+    name: String!
+    description: String
+    image: String
+    price: Int!
+}
+
+type Auth{
+    token: ID!
+    user: user
+}
+
+type Query{
+    me: User
+    cart:
+}
+
+type Mutation{
+    login(email:String, password:String): Auth
+    postItem(name:String, price:Int, image:String, description:String): User
+    updateItem(itemId:String, name:String, price:Int, image:String, description:String)
+    deleteItem(itemId:String): User
+    addToCart(itemId:String): User
+    removeFromCart(itemId:String): User
+}`;
+
+module.exports = typeDefs;
+
