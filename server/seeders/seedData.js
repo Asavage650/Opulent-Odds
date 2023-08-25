492457503dc21fd5b001d8c72d6d9ca95f17d962
Whynotwynaut <125845043+EHernandez1213@users.noreply.github.com>
2023-08-24 22:01:32 -0700

    added category model, and some seed data

---
+++ server/seeders/seedData.js
@@ -0,0 +1,66 @@
+const userData = [
+    {
+       username: 'DaveFree',
+       password: 'abcd1234',
+       email: 'davefree@gmail.com'
+    },
+    {
+        username: 'MonicaW',
+        password: 'dcba4321',
+        email: 'pokemonica@aol.com'
+    },
+    {
+        username: 'Jennyb',
+        password: 'asdf2345',
+        email: 'jfromtheblock@aol.com'
+    },
+    {
+        username: 'Lweis',
+        password: 'fdsaa214',
+        email: 'wiseLewis@aol.com'
+    },
+    {
+        username: 'vivXO',
+        password: 'qwerty123',
+        email: 'xotourfan@aol.com'
+    },
+    {
+        username: 'jeffgreen',
+        password: 'nbachamp2023',
+        email: 'jgreen32@aol.com'
+    },
+    {
+        username: 'PhillipJFry',
+        password: 'partylike2999',
+        email: 'futurama@aol.com'
+    },
+    {
+        username: 'ssammy',
+        password: '3456dfgh',
+        email: 'sammsamm@aol.com'
+    },
+    {
+        username: 'kevinkevins',
+        password: 'jklm1923',
+        email: 'okok@aol.com'
+    }
+];
+
+const categoryData = [
+    {
+        name: 'music'
+    },
+    {
+        name: 'clothing'
+    },
+    {
+        name: 'furniture'
+    },
+    {
+        name: 'art'
+    },
+    {
+        name: 'trinkets'
+    }
+];
+
