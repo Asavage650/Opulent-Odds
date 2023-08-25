855827525aaee4c469635b772cb0dde63d8c4c73
Cecilj24 <clangba@yahoo.com>
2023-08-24 21:58:31 -0700

    got the dropdown working

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../utils/Navbar.css"
-import {AiOutlineShoppingCart} from 'react-icons/ai'
-import {Link} from 'react-router-dom'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 function Navbar() {
+  const [showdropdown, setshowdropdown] = useState(false)
   return (
     <nav className='navbar'>
-    <Link to="/">Oppulent Odds</Link>
-  <ul className='list'>
-  <Link to="/">Home</Link>
-  <Link to="/Payment">Payment</Link>
-    <Link to="/Categories">Categories</Link> 
-    <Link to="/cart">
-      <AiOutlineShoppingCart size="2rem" />
-    </Link>
-  </ul>
-   
+      <Link className='link' to="/">Oppulent Odds</Link>
+      <ul className='list'>
+        <Link className='link' to="/">Home</Link>
+        <Link className='link' to="/Payment">Payment</Link>
+        <div style={{ position: "relative" }} onMouseEnter={() => setshowdropdown(true)} onMouseLeave={() => setshowdropdown(false)}>
+          <button className='button' >Categories</button>
+          {showdropdown && (
+            <motion.div
+              initial={{ opacity: 0, height: "0px" }}
+              animate={{ opacity: 1, height: "100%" }}
+              transition={{ duration: 1 }}
+            >
+              <ul className='' style={{ position: "absolute", top: "30px", backgroundColor: "black", width: "100%", textAlign: "center", display: "flex", flexDirection: "column" }}>
+                <li>Clothing</li>
+                <li>Antiques</li>
+                <li>Music</li>
+                <li>Clothing</li>
+              </ul>
+            </motion.div>
+          )}
+        </div>
+        <Link className='cart' to="/cart">
+          <AiOutlineShoppingCart size="2rem" />
+        </Link>
+      </ul>
+
+
     </nav>
   )
 }
