ca2083ef67ccea8fb6e62c039da5a4d3884a532e
{alexius <{alexiussavage0@gmail.com}>
2023-08-26 21:46:14 -0700

    creating pages with product cards

---
+++ client/src/pages/Shop/VinylPages.js
@@ -0,0 +1,83 @@
+import vin1 from "../../assets/images/vinyl/CurtisMayfieldSuperFlyVinyl.jpg";
+import vin2 from "../../assets/images/vinyl/FleetwoodMacRumoursVinyl.jpg";
+import vin3 from "../../assets/images/vinyl/MarvinGayeWhatsGoingOnVinyl.jpg";
+import vin4 from "../../assets/images/vinyl/MichaelJacksonThrillerVinyl.jpg";
+import vin5 from "../../assets/images/vinyl/NirvanaInUteroVinyl.jpg";
+import vin6 from "../../assets/images/vinyl/OutkastAqueminiVinyl.jpg";
+import vin7 from "../../assets/images/vinyl/PinkFloydDarkSideOfTheMoonVinyl.jpg";
+import vin8 from "../../assets/images/vinyl/PrincePurpleRainVinyl.jpg";
+import vin9 from "../../assets/images/vinyl/REOSpeedWagonHiInfidelityVinyl.jpg";
+import vin10 from "../../assets/images/vinyl/WeatherReportHeavyWeatherVinyl.jpg";
+
+export const VINYLPRODUCTS = [
+  {
+    id: 1,
+    productName: "Vinyl 1",
+    description: "Classic 1",
+    productImage: vin1,
+    price: 1000,
+  },
+  {
+    id: 2,
+    productName: "Vinyl 2",
+    description: "Classic 2",
+    productImage: vin2,
+    price: 1000,
+  },
+  {
+    id: 3,
+    productName: "Vinyl 3",
+    description: "Pretty piece 3",
+    productImage: vin3,
+    price: 1000,
+  },
+  {
+    id: 4,
+    productName: "Vinyl 4",
+    description: "Pretty piece 4",
+    productImage: vin4,
+    price: 1000,
+  },
+  {
+    id: 5,
+    productName: "Vinyl 5",
+    description: "Pretty piece 5",
+    productImage: vin5,
+    price: 1000,
+  },
+  {
+    id: 6,
+    productName: "Vinyl 6",
+    description: "Pretty piece 6",
+    productImage: vin6,
+    price: 1000,
+  },
+  {
+    id: 7,
+    productName: "Vinyl 7",
+    description: "Pretty piece 7",
+    productImage: vin7,
+    price: 1000,
+  },
+  {
+    id: 8,
+    productName: "Vinyl 8",
+    description: "Pretty piece 8",
+    productImage: vin8,
+    price: 1000,
+  },
+  {
+    id: 9,
+    productName: "Vinyl 9",
+    description: "Pretty piece 9",
+    productImage: vin9,
+    price: 1000,
+  },
+  {
+    id: 10,
+    productName: "Vinyl 10",
+    description: "Pretty piece 10",
+    productImage: vin10,
+    price: 1000,
+  },
+];
