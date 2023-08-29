6b31fbf6af5aa9e97daf50f6da5724213c0e0128
Cecilj24 <clangba@yahoo.com>
2023-08-28 19:52:08 -0700

    added sports page

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,12 @@ import Cart from "./pages/Cart1";
 import Homepage from "./pages/Homepage1";
 import Payment from "./pages/Payment1";
 // import Categories from "./pages/Categories";
-import Clothing from "./pages/Shop/Clothing";
 import Furniture from "./pages/Shop/Furniture";
 import Porcelan from "./pages/Shop/Porcelan";
 import Trinkets from "./pages/Shop/Trinkets";
 import Vinyls from "./pages/Shop/Vinyls";
 import Art from "./pages/Shop/Art";
+import SportsMem from "./pages/Shop/SportsMem";
 
 const client = new ApolloClient({
   uri: "/graphql",
@@ -31,7 +31,7 @@ function App() {
             <Route path="/cart" element={<Cart />} />
             <Route path="/Payment" element={<Payment />} />
             {/* <Route path="/Categories" element={<Categories />} /> */}
-            <Route path="/Clothing" element={<Clothing />} />
+            <Route path="/SportsMem" element={<SportsMem />} />
             <Route path="/Furniture" element={<Furniture />} />
             <Route path="/Porcelan" element={<Porcelan />} />
             <Route path="/Trinkets" element={<Trinkets />} />
---
+++ client/src/pages/Shop/Sportspages.js
@@ -0,0 +1,78 @@
+import mem1 from '../../assets/images/Memorobilia/mem1'
+import mem2 from '../../assets/images/Memorobilia/mem2'
+import mem3 from '../../assets/images/Memorobilia/mem3'
+import mem4 from '../../assets/images/Memorobilia/mem4'
+import mem5 from '../../assets/images/Memorobilia/mem5'
+import mem6 from '../../assets/images/Memorobilia/mem6'
+import mem7 from '../../assets/images/Memorobilia/mem7'
+import mem8 from '../../assets/images/Memorobilia/mem8'
+
+export const Memorobilia = [
+    {
+        id: 1,
+        productName: "Felix Bautisa bobblehead",
+        description: "antique mlb bobblehead",
+        productImage: mem1,
+        price: 100,
+
+    },
+    {
+        id: 2,
+        productName: "Joe Montana rookie card",
+        description: "antique nfl sports card",
+        productImage: mem2,
+        price: 10000,
+
+    },
+    {
+        id: 3,
+        productName: "Mickey Mantle rookie card",
+        description: "antique mlb sports card",
+        productImage: mem3,
+        price: 40000,
+
+    },
+    {
+        id: 4,
+        productName: "The amazing spiderman Original",
+        description: "Comic book",
+        productImage: mem4,
+        price: 100,
+
+    },
+    {
+        id: 5,
+        productName: "Street-fighter champion edition",
+        description: "OG Arcade game",
+        productImage: mem5,
+        price: 400,
+
+    },
+    {
+        id: 6,
+        productName: "Retro Jordan 1s Chicago",
+        description: "Jordan 1 sneakers",
+        productImage: mem6,
+        price: 1000,
+
+    },
+    {
+        id: 7,
+        productName: "OG Lakers jacket",
+        description: "Lakers 2000-2001 championship jacket",
+        productImage: mem7,
+        price: 2000,
+
+    },
+    {
+        id: 8,
+        productName: "G-Baby baseball jersey",
+        description: "Hardball movie jersey",
+        productImage: mem8,
+        price: 200,
+
+    },
+
+
+
+]
